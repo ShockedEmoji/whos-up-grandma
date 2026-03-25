04cefa2ee5155af1eecf2167c60529b6861e7d77
Thomas Olav <thomasolav.griffithjones@gmail.com>
2026-03-25 21:31:55 +1300

    skibidi npc

--- scripts/root.gd
+++ scripts/root.gd
@@ -46,8 +46,8 @@ func _ready():
 	_config_load_stuff()
 	
 	#_change_scene("menu/main_menu")
-	_change_scene("menu/credits")
-	#_change_scene("top_down/final_area")
+	#_change_scene("menu/credits")
+	_change_scene("top_down/waterfront")
 	#_change_scene("top_down/neo_bossfight")
 	#_change_scene("platformer/final_bossfight")
 	#_change_scene("top_down/mafia_bossfight")
