e56416dfe99a8c2bb06de728aac3a89e8cf28de5
PorcelainMug <porcelainmug.nz@gmail.com>
2026-03-26 23:10:24 +1300

    Funny things

--- scripts/root.gd
+++ scripts/root.gd
@@ -46,9 +46,9 @@ func _ready():
 	_config_load_stuff()
 	#
 	#_change_scene("menu/main_menu")
-	#_change_scene("top_down/neo_bossfight")
+	_change_scene("top_down/neo_bossfight")
 	#_change_scene("platformer/final_bossfight")
-	_change_scene("top_down/mafia_bossfight")
+	#_change_scene("top_down/mafia_bossfight")
 	
 	_fix_music_volume()
 	
