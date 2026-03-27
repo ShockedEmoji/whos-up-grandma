fc21d49f7940be154f8b1bd8d8e117b71259927d
PorcelainMug <porcelainmug.nz@gmail.com>
2026-03-27 21:25:43 +1300

    Wharf almost finito

--- scripts/root.gd
+++ scripts/root.gd
@@ -48,12 +48,12 @@ func _ready():
 	#_change_scene("menu/main_menu")
 	#_change_scene("menu/credits")
 	#_change_scene("top_down/grandma_house")
-	#_change_scene("top_down/school_3")
+	#_change_scene("top_down/school")
 	#_change_scene("top_down/final_area")
 	#_change_scene("top_down/waterfront")
 	#_change_scene("top_down/neo_bossfight")
 	#_change_scene("platformer/final_bossfight")
-	#_change_scene("top_down/mafia_bossfight")
+	_change_scene("top_down/mafia_bossfight")
 	
 	_fix_music_volume()
 	
