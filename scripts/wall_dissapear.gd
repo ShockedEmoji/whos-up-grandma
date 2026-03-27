f5c77cf7eb470df9dc40f63f5f25234137e083ee
Thomas Olav <thomasolav.griffithjones@gmail.com>
2026-03-27 16:53:08 +1300

    school wall dissapears

--- scripts/wall_dissapear.gd
+++ scripts/wall_dissapear.gd
@@ -9,16 +9,20 @@ func _process(_delta: float) -> void:
 	if player.position.y < y_position_to_dissapear && alpha_to_achieve == 1:
 		alpha_to_achieve = 0
 		_fade_away()
-	elif alpha_to_achieve == 0:
+	elif alpha_to_achieve == 0 && player.position.y > y_position_to_dissapear:
 		alpha_to_achieve = 1
 		_fade_in()
 
 func _fade_away():
 	var tween: Tween = create_tween()
 	
+	self.z_index = 2
+	
 	tween.tween_property(self, "modulate:a", 0, 1.0)
 
 func _fade_in():
 	var tween: Tween = create_tween()
+	
+	self.z_index = -2
 	
 	tween.tween_property(self, "modulate:a", 1, 1.0)
